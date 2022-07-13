ec755bb937c9fa4b39e73d92d1ae0f3beda5fd20
philippspohn <spohn.ph@gmail.com>
2022-07-13 17:27:49 +0200

    plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,9 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Service
 public class QueryProcessor {
 
@@ -13,8 +16,25 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else if(query.contains("what is 3 plus 13")){ // TODO extend the programm here
-            return "16";
+        }  else {
+            if(query.matches(".*what is [0-9]{1,5} plus [0-9]{1,5}.*")) {
+                Pattern p = Pattern.compile("-?\\d+");
+                Matcher m = p.matcher(query);
+                if(!m.find()) return "";
+                String m1 = m.group();
+                if(!m.find()) return "";
+                String m2 = m.group();
+
+                try {
+                    int i1 = Integer.parseInt(m1);
+                    int i2 = Integer.parseInt(m2);
+                    return String.valueOf(i1 + i2);
+                } catch (Exception e) {
+                    return "";
+                }
+
+            }
+            return "";
         }
     }
 }
