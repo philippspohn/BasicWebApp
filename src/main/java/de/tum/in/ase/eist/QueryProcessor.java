684c4f3aef7b21c84fd49b3fd5c11c80819129d1
philippspohn <spohn.ph@gmail.com>
2022-07-13 17:14:19 +0200

    erste Antwort

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,8 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "MyTeam";
-        } else { // TODO extend the programm here
-            return "";
+        } else if(query.contains("what is 3 plus 13")){ // TODO extend the programm here
+            return "16";
         }
     }
 }
